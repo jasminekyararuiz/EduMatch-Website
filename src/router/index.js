46bbfb7a10f781a5aef981da14e866aff5b1ae81
Jasmine Kyara Ruiz <jasminekyara@example.com>
2025-03-30 19:02:54 +0800

    feat: added login and register page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: LoginView
     },
+    {
+      path: '/register',
+      name: 'registration',
+      component: RegisterView 
+    }
+    
   ],
 })
 
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -26,27 +26,39 @@ function onClick() {
         <v-container>
           <v-row>
             <v-col cols="12" md="6" class="mx-auto">
-              <v-card class="mx-auto" prepend-icon=" mdi-login" subtitle="Login">
+              <v-card class="mx-auto" prepend-icon=" mdi-account-plus" subtitle="Sign Up">
                 <template v-slot:title>
                   <span class="font-weight-black">EduMatch</span>
                 </template>
 
                 <v-card-text class="bg-surface-light pt-4">
+                   
                   <v-sheet class="mx-auto">
                     <v-form fast-fail @submit.prevent>
+                        <v-row>
+                        <v-col>
+                            <v-text-field label="First Name" variant="outlined"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field label="Last Name" variant="outlined"></v-text-field>
+                        </v-col>
+                    </v-row>
                       <v-text-field label="Email" variant="outlined"></v-text-field>
-
                       <v-text-field
                         label="Password"
                         type="password"
                         variant="outlined"
                       ></v-text-field>
+                      <v-text-field
+                        label="Re-enter Password"
+                        type="password"
+                        variant="outlined"
+                      ></v-text-field>
 
-                      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+                      <v-btn class="mt-2" type="submit" block>Sign Up</v-btn>
                     </v-form>
-
                     <v-divider class="my-3" ></v-divider>
-                    <h5 class="text-center">Don't have account? <RouterLink class="text-primary" to="/register" >Click here to register </RouterLink> </h5>
+                    <h5 class="text-center">Already have an acoount? <RouterLink class="text-primary" to="/">Login</RouterLink></h5>
                   </v-sheet>
                 </v-card-text>
               </v-card>
