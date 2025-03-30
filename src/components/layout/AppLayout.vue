f8eb1ab71a17a121e3add11cfbacf005514116f2
Jasmine Kyara Ruiz <jasminekyara@example.com>
2025-03-31 01:16:49 +0800

    feat: updated applayout and views

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+<template>
+    <v-responsive class="border rounded">
+      <v-app :theme="theme">
+        <v-app-bar class="px-3" 
+        :color="theme === 'light' ? 'blue-lighten-3' : 'grey-darken-2'">
+          <v-spacer></v-spacer>
+  
+          <v-btn
+            :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+            variant="elevated"
+            slim
+            @click="onClick"
+          ></v-btn>
+        </v-app-bar>
+  
+        <v-main>
+          <v-container>
+            <slot name="content">
+
+            </slot>
+        </v-container>
+      </v-main>
+
+      <v-footer :color="theme === 'light' ? 'blue-lighten-5' : 'grey-darken-2'" border  app>
+        <span class="mx-auto">2025 - EduMatch</span>
+    </v-footer>
+    </v-app>
+  </v-responsive>
+</template>
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,38 +1,22 @@
 <script setup>
-import { ref } from 'vue'
+import AppLayout from '@/components/layout/AppLayout.vue';
+import { useDisplay } from 'vuetify'
 
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+const { mobile } = useDisplay()
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3" color="blue-lighten-3">
-        <v-spacer></v-spacer>
-
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
-
-      <v-main>
-        <v-container>
+<AppLayout>
+  <template #content>
           <v-row>
             <v-col cols="12" md="6" class="mx-auto">
-              <v-card class="mx-auto" prepend-icon=" mdi-login" subtitle="Login">
-                <template v-slot:title>
-                  <span class="font-weight-black">EduMatch</span>
-                </template>
+              <v-card class="mx-auto">
+                <v-card-title>
+                  <v-img class="mx-auto" src="/public/Edumatch_logo.png" width="25%"> </v-img>
+                  <h3 class="text-center"><b>EduMatch</b></h3>
+                </v-card-title>
 
                 <v-card-text class="bg-surface-light pt-4">
-                  <v-sheet class="mx-auto">
                     <v-form fast-fail @submit.prevent>
                       <v-text-field label="Email" variant="outlined"></v-text-field>
 
@@ -42,20 +26,16 @@ function onClick() {
                         variant="outlined"
                       ></v-text-field>
 
-                      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+                      <v-btn class="bg-primary" rounded="xl" type="submit" block>Log In</v-btn>
                     </v-form>
 
                     <v-divider class="my-3" ></v-divider>
                     <h5 class="text-center">Don't have account? <RouterLink class="text-primary" to="/register" >Click here to register </RouterLink> </h5>
-                  </v-sheet>
                 </v-card-text>
               </v-card>
             </v-col>
           </v-row>
-        </v-container>
-      </v-main>
+        </template>
 
-      <v-footer border color="blue-lighten-5" app>2025 - EduMatch</v-footer>
-    </v-app>
-  </v-responsive>
+</AppLayout>
 </template>
