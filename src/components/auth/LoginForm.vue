6407ade366661900d7a0c29e2217ec9bd138dfd2
leanheitminoza <leanheitminoza.github.io>
2025-04-07 23:41:46 +0800

    feat: add roles

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -4,6 +4,10 @@
 
     <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
 
+    <v-autocomplete ref="Role" v-model="Role" :items="['Tutors', 'Learners']"
+       :rules="[() => !!Role || 'This field is required']" label="Role" placeholder="Please select a role."
+       required></v-autocomplete>
+
     <v-btn class="bg-primary" rounded="xl" type="submit" block>Log In</v-btn>
   </v-form>
 </template>
