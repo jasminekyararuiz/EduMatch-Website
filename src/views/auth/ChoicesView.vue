7d2ac030da2b46f89fdedd16fb1dcec8dafd26a9
Jasmine Kyara Ruiz <jasminekyara@example.com>
2025-04-08 13:23:31 +0800

    fix the routerlink of choices view

--- src/views/auth/ChoicesView.vue
+++ src/views/auth/ChoicesView.vue
@@ -19,23 +19,21 @@ const { mobile } = useDisplay()
             </v-card-item>
 
             <v-card-text>
-              <v-row align="center" class="mx-0">
+              <v-row align="center" class="mx-0"> </v-row>
 
-              </v-row>
+              <div class="my-4 text-subtitle-1"></div>
 
-              <div class="my-4 text-subtitle-1">
-
+              <br />
+              <div class="text-center">
+                I’ll find the right tutor to help me understand my lessons better.
               </div>
-
-
-              <br>
-              <div class="text-center">I’ll find the right tutor to help me understand my lessons better.</div>
             </v-card-text>
 
             <v-divider class="mx-4 mb-1"></v-divider>
-
-            <v-card-actions>
-              <v-btn class="bg-primary" text="Learner Sign Up" block border @click="reserve"></v-btn>
+            <v-card-actions class="d-flex justify-center">
+              <RouterLink to="/learnersignup" class="text-decoration-none">
+                <v-btn class="bg-primary" text="Learner Sign Up" border @click="reserve"></v-btn>
+              </RouterLink>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -43,7 +41,12 @@ const { mobile } = useDisplay()
         <v-col cols="8" md="6">
           <v-card :disabled="loading" :loading="loading" class="mx-auto my-12" max-width="374">
             <template v-slot:loader="{ isActive }">
-              <v-progress-linear :active="isActive" color="deep-purple" height="4" indeterminate></v-progress-linear>
+              <v-progress-linear
+                :active="isActive"
+                color="deep-purple"
+                height="4"
+                indeterminate
+              ></v-progress-linear>
             </template>
 
             <v-img height="300" src="public/tutorr.png" cover></v-img>
@@ -53,21 +56,19 @@ const { mobile } = useDisplay()
             </v-card-item>
 
             <v-card-text>
-              <v-row align="center" class="mx-0">
+              <v-row align="center" class="mx-0"> </v-row>
 
-              </v-row>
-
-              <div class="my-4 text-subtitle-1">
-
-              </div>
-              <br>
+              <div class="my-4 text-subtitle-1"></div>
+              <br />
               <div class="text-center">I teach students and earn while sharing my knowledge.</div>
             </v-card-text>
 
             <v-divider class="mx-4 mb-1"></v-divider>
 
-            <v-card-actions>
-              <v-btn class="bg-primary" text="Tutor Sign Up" block border @click="reserve"></v-btn>
+            <v-card-actions class="d-flex justify-center">
+              <RouterLink to="/learnersignup" class="text-decoration-none">
+                <v-btn class="bg-primary" text="Tutor Sign Up" border @click="reserve"></v-btn>
+              </RouterLink>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -75,12 +76,7 @@ const { mobile } = useDisplay()
         <v-col class="2" md="1">
           <RouterLink class="text-primary" to="/">Back</RouterLink>
         </v-col>
-
-
       </v-row>
-
-
     </template>
   </AppLayout>
-
 </template>
--- src/views/auth/LearnersignupView.vue
+++ src/views/auth/LearnersignupView.vue
@@ -18,7 +18,21 @@ const { mobile } = useDisplay()
             </v-card-title>
 
             <v-card-text class="bg-surface-light pt-4">
-              <RegisterForm></RegisterForm>
+              <v-form fast-fail @submit.prevent>
+    <v-row>
+      <v-col>
+        <v-text-field label="First Name" variant="outlined"></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field label="Last Name" variant="outlined"></v-text-field>
+      </v-col>
+    </v-row>
+    <v-text-field label="Email" variant="outlined"></v-text-field>
+    <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+    <v-text-field label="Re-enter Password" type="password" variant="outlined"></v-text-field>
+
+    <v-btn class="bg-primary" rounded="xl" type="submit" block>Sign Up</v-btn>
+  </v-form>
 
               <v-divider class="my-3"></v-divider>
               <h5 class="text-center">
